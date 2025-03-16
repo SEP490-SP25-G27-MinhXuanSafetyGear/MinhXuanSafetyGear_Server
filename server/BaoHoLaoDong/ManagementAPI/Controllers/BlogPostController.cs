83168b2f2eb2938fdf2a129a9f4c695f9b525b2c
dinhlinh2002 <130349416+dinhlinh2002@users.noreply.github.com>
2025-03-16 10:55:10 +0700

    Update BlogPostController.cs

--- server/BaoHoLaoDong/ManagementAPI/Controllers/BlogPostController.cs
+++ server/BaoHoLaoDong/ManagementAPI/Controllers/BlogPostController.cs
@@ -15,7 +15,6 @@ public class BlogPostController : ControllerBase
    
     /// <summary>
     /// get blog categories
-    /// 
     /// </summary>
     /// <returns></returns>
     [HttpGet("get-blog-categories")]
